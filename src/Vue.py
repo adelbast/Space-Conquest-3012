0c696faeab7b7e785df6bbca54f6c51f27120b95
Arnaud Girardin <arnaud.girardin444@gmail.com>
2014-09-27 11:12:18 -0700

    Minimap avec Mouseclick et MouseMotion

--- src/Vue.py
+++ src/Vue.py
@@ -30,7 +30,7 @@ class Vue:
         print(self.relativeW, self.relativeH)
 
         #Image pour le HUD
-        self.imageHUD = Image.open("gui/gui.png")
+        self.imageHUD = Image.open("image/gui/gui.png")
         self.photoImageHUD = ImageTk.PhotoImage(self.imageHUD)
 
         #Pour transferer les images en PhotoImage
@@ -53,9 +53,14 @@ class Vue:
 
         #Pour que le canvas scroll lorsquon click
         self.root.bind("<Key>", self.scroll_move)
+
+        #Pour le click sur la map
+        self.miniMap.bind("<Button-1>", self.miniMapClick)
+        self.miniMap.bind("<B1-Motion>", self.miniMapClick)
         
     
 
+    #Deplacement de la map avec WASD
     def scroll_move(self, event):
         variation = 1
         
@@ -63,25 +68,53 @@ class Vue:
             print('up')
             self.surfaceJeu.yview('scroll', -variation, 'units')
 
-        if(event.char == 's'):
+        elif(event.char == 's'):
             print("down")
             self.surfaceJeu.yview('scroll', variation, 'units')
 
-        if(event.char == 'a'):
+        elif(event.char == 'a'):
             print("left")
             self.surfaceJeu.xview('scroll', -variation, 'units')
             
-        if(event.char == 'd'):
+        elif(event.char == 'd'):
             print("right")
             self.surfaceJeu.xview('scroll', variation, 'units')
 
         print(self.surfaceJeu.canvasx(0), self.surfaceJeu.canvasy(0))
 
         self.updateMiniMap()
-        
+
+    #Deplacer la camera lorsqu'on clique sur le canvas de la minimap
+    def miniMapClick(self, event):
+        print(event.x, event.y)
+
+        #Calcule le coin en haut a gauche du rectangle pour que le point clique soit le centre
+        posx = event.x-(self.relativeW/2)
+        posy = event.y-(self.relativeH/2)
+
+        if(posx < 0):
+            posx = 0
+            
+        elif(posx > self.miniMapW-(self.relativeW)):
+            posx = self.miniMapW-self.relativeW-1
+
+        if(posy < 0):
+            posy = 0
+            
+        elif(posy > self.miniMapH-(self.relativeH)):
+            posy = self.miniMapH-self.relativeH-1
+            
+
+        self.miniMap.delete("region")
+        self.miniMap.create_rectangle(posx, posy, posx + self.relativeW, posy + self.relativeH, outline='red', tags="region")
+
+        print((posx*(len(self.parent.modele.map.map[0])*64))/self.miniMapW, (posy*(len(self.parent.modele.map.map)*64))/self.miniMapH)
+
+        self.surfaceJeu.xview_moveto(posx*1/self.miniMapW)
+        self.surfaceJeu.yview_moveto(posy*1/self.miniMapH)
        
         
-
+    #Affiche la map
     def displayMap(self):
         
         #Pour chaque ligne
@@ -93,14 +126,17 @@ class Vue:
              
                 self.surfaceJeu.create_image(x*64,y*64,anchor=NW, image=self.parent.modele.tileset.tileset[int(self.parent.modele.map.map[y][x])].img, tags="tile")
 
-                
+
+    #Affiche le HUD
     def displayHUD(self):
         self.hud.create_image(0,0,anchor=NW,image=self.photoImageHUD, tags="hud")
         self.hud.place(x=0, y=550)
 
+    #Affiche la minimap a la bonne position
     def displayMiniMap(self):      
         self.miniMap.place(x=928, y=600)
-    
+
+    #Update la camera sur la minimap et aussi l'affichage des unites
     def updateMiniMap(self):
         self.miniMap.delete("region")
 
