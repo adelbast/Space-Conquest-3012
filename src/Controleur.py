4138fcd1cf3d2fa9ace286dd672745f5ce698434
xavierhd <1075569@408W09.decinfo.cvm>
2014-09-30 17:28:36 -0400

    Squelette Modele et Controleur

--- src/Controleur.py
+++ src/Controleur.py
@@ -1,11 +1,32 @@
 import Vue
 import Modele
+from Class import Server
+from Class import Client
 
 class Controleur:
     def __init__(self):
-        self.modele = Modele.Modele(self)
+        self.modele = Modele.Modele()
         self.vue = Vue.Vue(self)
+        self.client = None      #Client.Client()
+        self.serveur = None
         self.vue.root.mainloop()
+
+    def creeServer(self):
+        self.serveur = Serveur.Serveur(nomPartie,nomJoueur)
+
+    def lancerPartie(self):
+        self.modele.initPartie
+
+    def gameLoop(self):
+        self.modele.gestion()
+        #self.client.envoyerInfo(dic)
+        self.vue.root.after(24,self.gameLoop)
+
+    def gererMouseClick(self,event):
+        self.modele.gererMouseClic(event)
+
+    def gererMouseDrag(self,event):
+        self.modele.gererMouseDrag(event)
 
 if __name__ == "__main__":
     c = Controleur()
