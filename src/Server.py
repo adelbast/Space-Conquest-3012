b17b99339c62debc57b26cec0548e6e8a7b7ff20
xavierhd <xavierhd@hotmail.com>
2014-09-30 22:18:32 -0400

    Attention Alex...
    -Tu as overwrite deux fichiers dans ton dernier commit...
    -J'ai déplacé structure dans le dossier "Class"
    -Ajout des modification du commit "Afficher les Object(unit, structure,
    artefact)"

---
+++ src/Server.py
@@ -0,0 +1,45 @@
+# -*- coding: iso-8859-1 -*-
+import Pyro4
+import socket
+
+VERSION = "1.0"
+
+class Serviteur(object):
+	def __init__(self,nomServeur,nomJoueurHost,ip):
+		self.nomServeur = nomServeur
+		self.nomJoueurHost = nomJoueurHost
+		self.ip = ip
+
+	def servir(self):
+		pass
+
+	def getBasicInfo(self):
+		global VERSION
+		info = {
+			"NAME":self.nom,
+			"IP":self.ip,
+			"VERSION": VERSION,
+			"DEBUG":"No support",
+			"NB_CLIENT":"No support"
+		}
+		print("Trouver!")
+		return info
+
+        
+class Main(object):
+	def __init__(self, nomServeur,nomJoueurHost):
+		self.port = 9988
+		self.ip = socket.gethostbyname(socket.gethostname())
+		self.serviteur=Serviteur(nomServeur,nomJoueurHost,self.ip)
+
+		
+		daemon=Pyro4.Daemon(host=self.ip,port=self.port)
+
+		uri=daemon.register(self.serviteur,"SpaceConquest3012")
+		print(uri)
+
+		print("Pret!")
+		daemon.requestLoop()
+
+if __name__ == '__main__':
+	m=Main()
